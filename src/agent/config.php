ebc57afacd8f9557f98ec0235ccc995fcb8c8ced
watanabe-miyuki <kirin.myk2018@gmail.com>
2022-08-31 11:17:13 +0900

    ゴミ掃除　sqlの名前まだshukatsu

--- src/agent/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('DSN', 'mysql:host=db;dbname=shukatsu');
-define('DB_USER', 'posse_user');
-define('DB_PASS', 'password');
-?>
--- src/db_connect.php
+++ src/db_connect.php
@@ -5,21 +5,6 @@ function h($value) {
 	return htmlspecialchars((string)$value, ENT_QUOTES, "UTF-8");
 }
 
-function set_list_status($list_status)
-{
-  if ($list_status === 1) {
-    return '掲載中';
-  } elseif ($list_status === 2) {
-    return '掲載期間外';
-  } elseif ($list_status === 3) {
-    return '申込上限到達';
-  } elseif ($list_status === 4) {
-    return 'タグ不足';
-  } else {
-    return 'エラー';
-  }
-}
-
 $dsn = 'mysql:host=db;dbname=shukatsu;charset=utf8mb4;';
 $user = 'posse_user';
 $password = 'password';
